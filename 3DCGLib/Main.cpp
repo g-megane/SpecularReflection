20cbe0f09533e1961b2e909dc149a553e86dcfce
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-18 00:58:31 +0900

    メイン関数の作成

---
+++ 3DCGLib/Main.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
+
+    return 0;
+}
