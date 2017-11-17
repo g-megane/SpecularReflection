49b588ca785d20810c35da1d5e34a0e7db358f54
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-18 01:29:56 +0900

    ウィンドウの表示テスト

--- 3DCGLib/Main.cpp
+++ 3DCGLib/Main.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include "Window.h"
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
@@ -6,6 +7,14 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
     UNREFERENCED_PARAMETER(nCmdShow);
+
+    Lib::Window w = Lib::Window(L"hoge");
+
+    while (w.Update().message != WM_QUIT) {
+
+    }
+
+   
 
     return 0;
 }
