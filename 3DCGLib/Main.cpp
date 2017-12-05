034c71c5316cd8a004f487a62be9b0ddbaefeee9
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-12-06 02:14:54 +0900

    モデルをキー入力で回転するように変更

--- 3DCGLib/Main.cpp
+++ 3DCGLib/Main.cpp
@@ -3,7 +3,7 @@
 #include "DirectX11.h"
 #include "Model.h"
 #include "Matrix.h"
-#include "MathConstantValues.h"
+#include "MyMath.h"
 
 using namespace Lib;
 
@@ -14,44 +14,64 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     UNREFERENCED_PARAMETER(lpCmdLine);
     UNREFERENCED_PARAMETER(nCmdShow);
 
+    // ウィンドウの作成
     auto w = std::make_shared<Lib::Window>(L"3DCGLib", 1026, 768);
 
+    // DirectXのインスタンスの取得
     auto& directX = Lib::DirectX11::getInstance();
     directX.initDevice(w);
 
     // ViewMatrixの初期化
     Vector3 eye = Vector3(0.0f, 1.0f, -5.0f); // カメラの座標
     Vector3 at  = Vector3(0.0f, 1.0f,  0.0f); // 注視対象
-    Vector3 up  = Vector3(0.0f, 1.0f,  0.0f); // 現在のワールド座標の上方向
+    Vector3 up  = Vector3::UP;                // 現在のワールド座標の上方向
     auto view   = Matrix::LookAtLH(eye, at, up);
     directX.setViewMatrix(view);
 
     // ProjectionMatrixの初期化
     LONG windowWidth  = w->getWindowRect().right  - w->getWindowRect().left;
     LONG windowHeight = w->getWindowRect().bottom - w->getWindowRect().top;
-    auto projection   = Matrix::perspectiveFovLH(PIDIV2, windowWidth / static_cast<float>(windowHeight), 0.01f, 100.0f);
+    auto projection   = Matrix::perspectiveFovLH(MyMath::PIDIV2, windowWidth / static_cast<float>(windowHeight), 0.01f, 100.0f);
     directX.setProjectionMatrix(projection);
     
+    // モデルの作成
     Model model = Model();
     Matrix world;
     world = Matrix::Identify;
 
+    float rotX = 0.0f;
+    float rotY = 0.0f;
+
+    // 更新処理
     while (w->Update().message != WM_QUIT) {
         directX.begineFrame();
 
         // 回転
-        static float t = 0.0f;
-        static DWORD dwTimeStart = 0;
-        DWORD dwTimeCur = GetTickCount();
-        if (dwTimeStart == 0)
-            dwTimeStart = dwTimeCur;
-        t = (dwTimeCur - dwTimeStart) / 1000.0f;
+        if (w->getKeyDown('W')) {
+            rotX += MyMath::PIDIV2 / 1800.0f;
+        }
+        if (w->getKeyDown('S')) {
+            rotX -= MyMath::PIDIV2 / 1800.0f;
+        }
+        if (w->getKeyDown('A')) {
+            rotY += MyMath::PIDIV2 / 1800.0f;
+        }
+        if (w->getKeyDown('D')) {
+            rotY -= MyMath::PIDIV2 / 1800.0f;
+        }
 
-        auto mtr = Matrix::rotateY(t);
-        auto mtt = Matrix::translate(Vector3(0.0f, -1.0f, 0.0f));
-        world = mtr * mtt;
+        // オーバーフローの制御
+        rotX = MyMath::rollup<float>(rotX, MyMath::PI2);
+        rotY = MyMath::rollup<float>(rotY, MyMath::PI2);
 
+        // モデルの制御
+        auto mtrX = Matrix::rotateX(rotX);
+        auto mtrY = Matrix::rotateY(rotY);
+        auto mtt  = Matrix::translate(Vector3(0.0f, -1.0f, 0.0f));
+        world = mtrX * mtrY * mtt;
         model.setWorldMatrix(world);
+
+        // 描画
         model.render(Lib::Color(Lib::Color::BLUE));
 
         directX.endFrame();
