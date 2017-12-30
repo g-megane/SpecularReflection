7855eb315ddc83e34cd72c23fbe1e3b7f3912f7b
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-12-31 01:56:58 +0900

    頂点シェーダ－の変更
    ・ワールド変換した状態の座標を保持するPosWを定義

--- 3DCGLib/VertexShader.hlsl
+++ 3DCGLib/VertexShader.hlsl
@@ -4,9 +4,10 @@ cbuffer ConstantBuffer : register(b0)
     matrix World;          // ワールド行列
     matrix View;           // ビュー行列
     matrix Projection;     // 射影行列
-    float4 vLightDir[2];   // ライトの座標
-    float4 vLightColor[2]; // ライトの色
-    float4 vOutputColor;   // ライト以外のモデルの色
+    float4 vEyePos;
+    float4 vLightDir;      // ライトの座標
+    float4 vLightColor;    // ライトの色
+    float4 vSpecular;   // ライト以外のモデルの色
 }
 
 struct VS_INPUT
@@ -18,17 +19,18 @@ struct VS_INPUT
 struct PS_INPUT
 {
     float4 Pos : SV_POSITION;
-    float3 Norm : TEXCOORD0;
+    float4 PosW : POSITION0;
+    float4 NorW : TEXCOORD0;
 };
 
 // 頂点シェーダー
 PS_INPUT VS(VS_INPUT input)
 {
     PS_INPUT output = (PS_INPUT)0;
-    output.Pos  = mul(input.Pos, World);
-    output.Pos  = mul(output.Pos, View);
-    output.Pos  = mul(output.Pos, Projection);
-    output.Norm = mul(float4(input.Norm, 0.0f), World).xyz;
+    output.PosW  = mul(input.Pos, World);
+    output.Pos   = mul(output.PosW, View);
+    output.Pos   = mul(output.Pos, Projection);
+    output.NorW  = mul(input.Norm, World);
 
     return output;
 }
