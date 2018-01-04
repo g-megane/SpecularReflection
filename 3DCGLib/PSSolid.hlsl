9d2fe524c9318ca69a75c09b3f5a364a080a4ae1
Ryota Goto <pandoraxssscan.929@gmail.com>
2018-01-04 14:04:30 +0900

    PSSolightの不要な部分の削除

--- 3DCGLib/PSSolid.hlsl
+++ 3DCGLib/PSSolid.hlsl
@@ -1,18 +1,20 @@
 // コンスタントバッファ
 cbuffer ConstantBuffer : register(b0)
 {
-    matrix World;          // ワールド行列
-    matrix View;           // ビュー行列
-    matrix Projection;     // 射影行列
-    float4 vLightDir[2];   // ライトの座標
-    float4 vLightColor[2]; // ライトの色
-    float4 vOutputColor;   // ライト以外のモデルの色
+    matrix World;       // ワールド行列
+    matrix View;        // ビュー行列
+    matrix Projection;  // 射影行列
+    float4 vEyePos;     // 視点の座標
+    float4 vLightDir;   // ライトの座標
+    float4 vLightColor; // ライトの色
+    float4 vSpecular;   // ライト以外のモデルの色
 }
 
 struct PS_INPUT
 {
     float4 Pos : SV_POSITION; // 現在のピクセル位置
-    float3 Norm : TEXCOORD0;  // 法線
+    float4 PosW : POSITION0;  // オブジェクトのワールド座標
+    float4 NorW : TEXCOORD0;  // 法線
 };
 
 // ピクセルシェーダー(ライティングなし)
